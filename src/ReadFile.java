36d894635d44f524716d1267ab81b942a1951e3e
Ermac <ermac.e@yandex.ru>
2022-08-30 12:34:47 +0300

    Ermachenkov hw sprint_2 fix readFile

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -2,9 +2,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+
 public class ReadFile {
     //Использовал полный путь, т.к. в моей системе не отрабатывает более короткий
-    String path = "C:\\Users\\eermachenkov\\dev\\second-project\\java-sprint2-hw\\resources\\";
+    String path = "java-sprint2-hw/resources/";
     public String readFileContentsOrNull(String path)
     {
         path = this.path + path;
